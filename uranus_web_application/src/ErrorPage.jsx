4aee724292b2439b846bb55ed4176cb6c6e79f12
alyamani edrees <git.generalassemb.ly>
2020-04-19 10:19:47 +0300

    added front end routes and errorpage

---
+++ uranus_web_application/src/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ErrorPage.css";
+export default function ErrorPage() {
+  return (
+    <div className="ErrorPage">
+      <h1>404</h1>
+      <h3>Looks Like You Are Lost!</h3>
+    </div>
+  );
+}
